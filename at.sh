fa109ec085c69292544aa0bd820de04725bbfcdc
trinadhsaipampana <trinadhsaipampana@gmail.com>
2024-08-22 19:14:42 +0000

    adding linuxPracticeFiles to git

---
+++ at.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo $0
+
+echo "Hi" >> /home/ubuntu/linuxFiles/linuxPracticeFiles/at.txt
---
+++ cron.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo $0
+
+echo "Hi" >> /home/ubuntu/linuxFiles/linuxPracticeFiles/cron.txt
